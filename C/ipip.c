dd9a40b44f96cf4e6e61b4e85245e3e50c79ce37
Akulakand87 <116587040+Akulakand87@users.noreply.github.com>
2023-10-25 20:22:45 +0530

    C

---
+++ C/ipip.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned char byte;
+typedef unsigned int uint;
+#define B2IL(b) (((b)[0] & 0xFF) | (((b)[1] << 8) & 0xFF00) | (((b)[2] << 16) & 0xFF0000) | (((b)[3] << 24) & 0xFF000000))
+#define B2IU(b) (((b)[3] & 0xFF) | (((b)[2] << 8) & 0xFF00) | (((b)[1] << 16) & 0xFF0000) | (((b)[0] << 24) & 0xFF000000))
+
+struct {
+    byte *data;
+    byte *index;
+    uint *flag;
+    uint offset;
+} ipip;
+
+int destroy() {
+    if (!ipip.offset) {
+        return 0;
+    }
+    free(ipip.flag);
+    free(ipip.index);
+    free(ipip.data);
+    ipip.offset = 0;
+    return 0;
+}
+
+int init(const char *ipdb) {
+    if (ipip.offset) {
+        return 0;
+    }
+    FILE *file = fopen(ipdb, "rb");
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    ipip.data = (byte *) malloc(size * sizeof(byte));
+    size_t r = fread(ipip.data, sizeof(byte), (size_t) size, file);
+
+    if (r == 0) {
+        return 0;
+    }
+
+    fclose(file);
+
+    uint length = B2IU(ipip.data);
+
+    ipip.index = (byte *) malloc(length * sizeof(byte));
+    memcpy(ipip.index, ipip.data + 4, length);
+
+    ipip.offset = length;
+
+    ipip.flag = (uint *) malloc(256 * sizeof(uint));
+    memcpy(ipip.flag, ipip.index, 256 * sizeof(uint));
+
+    return 0;
+}
+
+int find(const char *ip, char *result) {
+    {
+        
+    }
+    return 0;
+}
+
